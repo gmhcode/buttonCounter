19150a016a5cc20232dcfa7d02610de7db82f2c5
GMHCode <greghughes988@gmail.com>
2018-11-30 14:50:33 -0700

    Initial Commit

---
+++ gitPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitPractice
+//
+//  Created by Greg Hughes on 11/30/18.
+//  Copyright © 2018 Greg Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
